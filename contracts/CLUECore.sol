83c9e6401b32fc44b960c5d22ed422899f04b90f
Terence Pae <terencepae@gmail.com>
2018-12-17 00:03:33 -0800

    updated ownership and added openzepplin

--- contracts/CLUECore.sol
+++ contracts/CLUECore.sol
@@ -1,8 +1,9 @@
 pragma solidity ^0.4.23;
 
-import "./CLUEBase.sol"
+import 'openzeppelin-solidity/contracts/ownership/Ownable.sol';
+import './CLUEBase.sol';
 
-contract CLUECore is CLUEBase {
+contract CLUECore is CLUEBase, Ownable {
   // Set in case the core contract is broken and an upgrade is required
   address public newContractAddress;
 
